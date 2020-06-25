b52d3bdb7c20798d9381d9d32a63097bfd99c388
A. Matías Quezada <amatiasq@gmail.com>
2020-06-26 01:13:10 +0200

    feat(bots): using descord-bot

--- bots/main.ts
+++ bots/main.ts
@@ -1,28 +1,23 @@
 import { config } from 'https://deno.land/x/dotenv/mod.ts';
-import { connectDiscord, DiscordEvent, Intent } from '../descord/mod.ts';
+import { Bot, Intent } from '../descord-bot/mod.ts';
 
 const { MARTI_BOT_ID, MARTI_TOKEN } = config();
 
-const client = await connectDiscord(MARTI_BOT_ID, MARTI_TOKEN, [
-	Intent.GUILDS,
-	Intent.GUILD_MESSAGES,
-	Intent.GUILD_MESSAGE_REACTIONS,
-]);
-
-// client.onMessage(x => {
-// 	if (x.op === GatewayOpCode.Dispatch) {
-// 		console.log(x.t, x.d);
-// 	}
-// });
-
-client.onDispatch(DiscordEvent.MESSAGE_CREATE, x => {
-	if (x.author.id === MARTI_BOT_ID) {
-		return;
-	}
-
-	client.api.createMessage(x.channelId, {
-		content: 'potat',
-	});
+const bot = new Bot({
+	id: MARTI_BOT_ID,
+	token: MARTI_TOKEN,
+	names: ['marti'],
+	intents: [
+		Intent.GUILDS,
+		Intent.GUILD_MESSAGES,
+		Intent.GUILD_MESSAGE_REACTIONS,
+	],
 });
 
-console.log('READY');
+bot.middleware(x => {
+	x.reply('FOO');
+	console.log(x);
+	return false;
+});
+
+await bot.connect();
