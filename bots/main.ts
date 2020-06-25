c3dcbc03ec6feaca3b8a4a678781b231da569bc2
A. Matías Quezada <amatiasq@gmail.com>
2020-06-25 23:18:20 +0200

    feat(bots): answer to every message

--- bots/main.ts
+++ bots/main.ts
@@ -1,17 +1,28 @@
-// import './bots/Marti.ts';
 import { config } from 'https://deno.land/x/dotenv/mod.ts';
-import { connectDiscord, Intent, GatewayOpCode } from '../descord/mod.ts';
+import { connectDiscord, DiscordEvent, Intent } from '../descord/mod.ts';
 
-const env = config();
+const { MARTI_BOT_ID, MARTI_TOKEN } = config();
 
-const client = await connectDiscord('718001826200420415', env.MARTI_TOKEN, [
+const client = await connectDiscord(MARTI_BOT_ID, MARTI_TOKEN, [
 	Intent.GUILDS,
 	Intent.GUILD_MESSAGES,
 	Intent.GUILD_MESSAGE_REACTIONS,
 ]);
 
-client.onMessage(x => {
-	if (x.op === GatewayOpCode.Dispatch) {
-		console.log(x.t, x.d);
+// client.onMessage(x => {
+// 	if (x.op === GatewayOpCode.Dispatch) {
+// 		console.log(x.t, x.d);
+// 	}
+// });
+
+client.onDispatch(DiscordEvent.MESSAGE_CREATE, x => {
+	if (x.author.id === MARTI_BOT_ID) {
+		return;
 	}
+
+	client.api.createMessage(x.channelId, {
+		content: 'potat',
+	});
 });
+
+console.log('READY');
