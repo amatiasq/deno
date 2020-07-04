6ceb4f0087ffd6174835d97688d1f402c27e8e30
A. Matías Quezada <amatiasq@gmail.com>
2020-07-04 18:39:27 +0200

    feat(amq/string): normalize removes diacritics

--- amq/string.ts
+++ amq/string.ts
@@ -1,9 +1,11 @@
+import { normalizeSync as removeDiacritics } from 'https://cdn.jsdelivr.net/gh/motss/normalize-diacritics@2.11.1/src/normalize-diacritics.ts';
+
 export function containsWord(text: string, word: string) {
 	return new RegExp(`\\b${word}\\b`).test(normalize(text));
 }
 
 export function normalize(text: string) {
-	return text.toLowerCase().normalize();
+	return removeDiacritics(text.toLowerCase());
 }
 
 export function padLeft(value: string | number, length = 2, fill = '0') {
