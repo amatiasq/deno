a4cf686bcca3e11398d5e87de7dd06b2e78ee037
A. Matías Quezada <amatiasq@gmail.com>
2020-07-04 09:26:26 +0200

    fix(bots): use default message for command 'encuesta'

--- bots/commands/encuesta.ts
+++ bots/commands/encuesta.ts
@@ -3,7 +3,7 @@ import { wait } from '../../amq/promise.ts';
 import { Bot, BotMessage } from '../../denord-bot/mod.ts';
 
 export default async function (message: BotMessage, bot: Applied<typeof Bot>) {
-	const poll = await message.channel.send(message.clean);
+	const poll = await message.channel.send(message.clean || 'Si o no?');
 	const react = (emoji: string) =>
 		bot.api?.createReaction(message.channelId, poll.id, emoji);
 
